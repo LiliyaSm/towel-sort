fc2097efe0841928e1737289bff547e4901b163d
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-09-29 10:35:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (matrix) {
+        for (let i = 0; i < matrix.length; i++) {
+            //odd line 1, 3, 5 => add items to result in reverse order.
+            if (i % 2) {
+                matrix[i].reverse().forEach((v) => result.push(v));
+            } else {
+                matrix[i].forEach((v) => result.push(v));
+            }
+        }
+    }
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    return result;
+};
